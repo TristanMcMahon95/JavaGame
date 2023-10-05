5d1e4c7483dc62211ddc07077be034305e074980
TristanMcMahon95 <tristan.mcmahon95@gmail.com>
2023-10-05 18:19:57 -0400

    Blank Black Screen

---
+++ src/main/GamePanel.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // pixels
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    Thread gameThread;
+    public  GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+    }
+    @Override
+    public void run() {
+
+    }
+}
